9a7999eeac54dc913f208de6f6e320d10d4c035b
mdalbrid <mdalbrid@student.21-school.ru>
2020-11-16 18:50:48 +0300

    done 2

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mdalbrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 12:24:28 by mdalbrid          #+#    #+#             */
-/*   Updated: 2020/11/16 14:22:15 by mdalbrid         ###   ########lyon.fr   */
+/*   Updated: 2020/11/16 18:49:13 by mdalbrid         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,24 @@ size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t a;
 	size_t lnd;
+	size_t lns;
 
-	a = dstsize;
+	a = 0;
 	lnd = ft_strlen(dst);	
+	lns = ft_strlen(src);
 	if (dst == NULL && src == NULL)
 		return (0);
 	if (dstsize == 0)
 		return (ft_strlen(src));
+	if (lnd >= dstsize)
+		return (ft_strlen(src) + dstsize);
 	while (*dst != '\0')
 		dst++;
-	while (*src != '\0' && lnd < dstsize--)
+	while (*src != '\0' && a + lnd < dstsize - 1)
 	{
 		*dst++ = *src++;
-		// dstsize -= 1;;
+		a++;
 	}
-	if (dstsize == lnd)
-		dst--;
 	*dst = '\0';
-	return (a + ft_strlen(src));
+	return (lnd + lns);
 }
